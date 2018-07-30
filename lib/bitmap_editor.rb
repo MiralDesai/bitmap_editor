2cef4e9b5083ad9b5b9fb3f2d54b7202aae2127e
Miral Desai <miral.desai4@gmail.com>
2018-07-30 20:08:46 +0100

    Added logic for H and V command, and added more validation at image level

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,36 +1,58 @@
 # frozen_string_literal: true
 
 require './lib/image.rb'
-require 'pry'
 
 class BitmapEditor
   def run(file)
-    return puts 'please provide correct file' if file.nil? || !File.exist?(file)
+    raise 'please provide correct file' if file.nil? || !File.exist?(file)
+    start(file)
+  end
+
+  private
+
+  def start(file)
     File.open(file).each do |line|
-      whatever = parse_line(line)
-      params = sanitize_params(whatever)
+      raw_params = parse_line(line)
+      params = sanitize_params(raw_params)
       command = params[0]
       case command
       when 'S'
+        check_image_created(command)
         puts image.pretty_print
       when 'I'
         @image = Image.new(column: params[1], row: params[2])
       when 'L'
-        # Cleanup params before passing to class?
-        image.set_colour_at(column: params[1], row: params[2], colour: params[3])
+        check_image_created(command)
+        image.set_colour_at(
+          column: params[1],
+          row: params[2],
+          colour: params[3]
+        )
       when 'V'
-        # Set color for vertical line between coords
+        check_image_created(command)
+        image.paint_line(
+          column: params[1],
+          row_start: params[2],
+          row_end: params[3],
+          colour: params[4],
+          direction: command
+        )
       when 'H'
-        # Set color for horizontal line between coords
+        check_image_created(command)
+        image.paint_line(
+          column: params[1],
+          row_start: params[2],
+          row_end: params[3],
+          colour: params[4]
+        )
       when 'C'
+        check_image_created(command)
         image.clear
       else
         puts "Unrecognised command: #{params}"
       end
     end
   end
-
-  private
 
   def parse_line(line)
     line.chomp.split(' ')
@@ -41,14 +63,18 @@ class BitmapEditor
     params.map do |param|
       raise 'Unknown parameter, please check file' unless param.match(/[a-z]/) || param.match(/[A-Z]/) || param.match(/\d{1,3}/)
       if param =~ /\d{1,3}/
-        raise 'Coordinates must be between 0 and 250' unless param.to_i.between?(0, 250)
-        param.to_i
+        raise 'Coordinates must be between 1 and 250' unless param.to_i.between?(1, 250)
+        param.to_i - 1
       elsif param =~ /[a-z]/
         param.upcase
       else
         param
       end
     end
+  end
+
+  def check_image_created(command)
+    raise "Cannot perform #{command} without creating image first" unless image
   end
 
   attr_accessor :image
--- lib/image.rb
+++ lib/image.rb
@@ -13,10 +13,11 @@ class Image
     image = ''
     @column.times do |column|
       @row.times do |row|
-        image << tiles[column][row].value
+        image = image.dup << tiles[column][row].value
       end
-      image << "\n"
+      image = image.dup << "\n"
     end
+    image = image.dup << "\n"
     image
   end
 
@@ -27,13 +28,49 @@ class Image
   end
 
   def set_colour_at(column:, row:, colour:)
+    validate_position(column, row)
     tiles[column][row].value = colour
+  end
+
+  def paint_line(column:, row_start:, row_end:, colour:, direction: 'H')
+    case direction
+    when 'H'
+      validate_row(row_start)
+      validate_row(row_end)
+      validate_column(column)
+      (row_start..row_end).each do |current_row|
+        tiles[column][current_row].value = colour
+      end
+    else
+      validate_column(row_start)
+      validate_column(row_end)
+      validate_row(column)
+      (row_start..row_end).each do |current_row|
+        tiles[current_row][column].value = colour
+      end
+    end
   end
 
   private
 
+  def validate_row(row)
+    raise "Invalid row: #{row}" unless row.between?(1, @row)
+  end
+
+  def validate_column(column)
+    raise "Invalid column: #{column}" unless column.between?(1, @column)
+  end
+
+  def validate_position(column, row)
+    raise "Invalid selection for column: #{column} and row: #{row}" if invalid_tile?(column, row)
+  end
+
+  def invalid_tile?(column, row)
+    tiles.dig(column, row).nil?
+  end
+
   def create
-    @tiles = Array.new(column) { |column| Array.new(row) { |row| Tile.new(row: row, column: column) } }
+    @tiles = Array.new(column) { |column| Array.new(row) { |row| Tile.new(column: column, row: row) } }
   end
 
   attr_accessor :column, :row, :tiles
